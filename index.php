c93b239538c546295880a07b45c44426430512da
JohannesJoujo <jagj2723@gmail.com>
2023-03-13 21:27:21 +0100

    Initial commit

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 require_once "comment.php";
-
-if (isset($_POST["radera"])) {
-    $delcomment = new comment("","");
-    $delcomment->deleteComment($_POST["radera"]);
-}
+require_once "delete.php";
+require_once "includes/add.php";
 ?>
 
     <!DOCTYPE html>
